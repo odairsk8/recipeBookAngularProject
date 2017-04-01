077db4bafcc00efc94ed4af0a56a2a2230437927
Odair Ferreira <odairsk8@hotmail.com>
2017-04-01 10:33:22 +1000

    adding bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recipe/recipe.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rb-recipes',
+  templateUrl: './recipe.component.html'
+})
+export class RecipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
