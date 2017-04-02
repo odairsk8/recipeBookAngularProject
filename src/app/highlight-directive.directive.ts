3a4252d8ca7661ef65cc1979d1363e2460cd4da8
Odair Ferreira <odairsk8@hotmail.com>
2017-04-02 22:02:00 +1000

    tests

--- src/app/highlight-directive.directive.ts
+++ src/app/highlight-directive.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer } from '@angular/core';
 
 @Directive({
   selector: '[rbHighlightDirective]'
 })
 export class HighlightDirectiveDirective {
 
-  constructor() { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer) { 
+    //console.log(this.renderer);
+    this.elementRef.nativeElement.style.backgroungColor = 'blue';
+    //this.renderer.setElementStyle(this.elementRef,"background-color", "blue");
+  }
 
 }
