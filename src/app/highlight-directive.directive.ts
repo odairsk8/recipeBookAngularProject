54b6b626d1a306c3efbe2caa41d92792af115ae3
Odair Ferreira <odairsk8@hotmail.com>
2017-04-02 22:14:45 +1000

    checkinh host options

--- src/app/highlight-directive.directive.ts
+++ src/app/highlight-directive.directive.ts
@@ -1,14 +1,28 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[rbHighlightDirective]'
 })
 export class HighlightDirectiveDirective {
 
+  @HostListener('mouseenter') onmouseover(){
+    this.backgroundColor = 'yellow';
+  }
+
+  @HostListener('mouseout') onmouseout(){
+    this.backgroundColor = 'blue';
+  }
+
+  @HostBinding('style.backgroundColor') get setColor(){
+    return this.backgroundColor;
+  }
+
+  private backgroundColor = 'blue';
+
   constructor(private elementRef: ElementRef, private renderer: Renderer) { 
     //console.log(this.renderer);
-    this.elementRef.nativeElement.style.backgroungColor = 'blue';
-    //this.renderer.setElementStyle(this.elementRef,"background-color", "blue");
+    //this.elementRef.nativeElement.style.backgroungColor = 'blue';
+    this.renderer.setElementStyle(this.elementRef.nativeElement,"background-color", this.backgroundColor);
   }
 
 }
